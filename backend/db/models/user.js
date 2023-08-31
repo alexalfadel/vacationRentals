df39260e16e7de31f5eef3c6f8c446e837d021b4
Alex Alfadel <alexalfadel@gmail.com>
2023-08-30 17:18:34 -0700

    Add firstName and lastName to User and update api routes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -25,6 +25,12 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
+    firstName: {
+      type: DataTypes.STRING
+    },
+    lastName: {
+      type: DataTypes.STRING
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
