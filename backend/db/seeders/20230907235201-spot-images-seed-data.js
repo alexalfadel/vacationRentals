42183004a7b9723c3d958c17bf3e39dec0dce4cf
Alex Alfadel <alexalfadel@gmail.com>
2023-09-07 16:56:12 -0700

    seed data added for spot images

---
+++ backend/db/seeders/20230907235201-spot-images-seed-data.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { SpotImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await SpotImage.bulkCreate([
+    {
+      url: 'images.com/spotimage1',
+      preview: true,
+      spotId: 1
+    },
+    {
+      url: 'images.com/spotimage2',
+      preview: true,
+      spotId: 2
+    },
+    {
+      url: 'images.com/spotimage3',
+      preview: false,
+      spotId: 3
+    }
+   ], { validate: true } )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'SpotImages'
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelet(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {})
+  }
+};
