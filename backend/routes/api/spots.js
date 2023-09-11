c9880e1d69a5aadec327a1bc06d404f3e44b2740
Alex Alfadel <alexalfadel@gmail.com>
2023-09-11 14:33:02 -0700

    created route to get all spots for the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,6 +4,82 @@ const { Review, SpotImage } = require('../../db/models')
 const { requireAuth } = require('../../utils/auth')
 const router = express.Router();
 
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id;
+
+    const allUserSpots = await Spot.findAll({
+        where: {
+            ownerId: userId
+        }
+    })
+
+    let allUserSpotsRes = {
+        Spots: []
+    };
+
+    //iterating through each spot to create each spot object
+    for (let i = 0; i < allUserSpots.length; i++) {
+        const spot = allUserSpots[i];
+        //getting all reviews associated with each spot
+        const reviews = await Review.findAll({
+            where: {
+                spotId: spot.id
+            }
+        });
+
+        //calculating average rating for each spot
+
+        let avgRating;
+
+        if (!reviews.length) avgRating = "No reviews yet";
+        else {
+            let total = 0;
+
+            reviews.forEach(review => {
+                total += review.dataValues.stars
+            })
+    
+            avgRating = total / reviews.length;
+        }
+
+        const spotPreviewImage = await SpotImage.findAll({
+            where : {
+                spotId: spot.id,
+                preview: true
+            }
+        })
+
+        let imageUrl;
+
+        if (spotPreviewImage[0]) {
+            imageUrl = spotPreviewImage[0].dataValues.url;
+        } else imageUrl = 'none'
+
+        const spotObj = {
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            avgRating: avgRating,
+            previewImage: imageUrl
+        };
+
+        allUserSpotsRes.Spots.push(spotObj)
+    }
+
+    return res.status(200).json(allUserSpotsRes);
+
+})
+
 router.get('/', async (req, res) => {
     //getting all of the spots
     const spots = await Spot.findAll();
@@ -11,7 +87,6 @@ router.get('/', async (req, res) => {
     let allSpots = {
         Spots: []
     }
-
 
     //iterating through each spot to create each spot object
     for (let i = 0; i < spots.length; i++) {
@@ -41,11 +116,8 @@ router.get('/', async (req, res) => {
         let imageUrl;
 
         if (spotPreviewImage[0]) {
-                // console.log('IT EXISTS!!!!!')
             imageUrl = spotPreviewImage[0].dataValues.url;
         } else imageUrl = 'none'
-
-        console.log(spotPreviewImage[0])
 
         //creating each spot object
         const spotObj = {
