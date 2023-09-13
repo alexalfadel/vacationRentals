a294d5d8d311d82203e2e601a2f573d5b469e822
Alex Alfadel <alexalfadel@gmail.com>
2023-09-12 17:05:58 -0700

    validation errors added for bookings

--- backend/app.js
+++ backend/app.js
@@ -262,6 +262,12 @@ app.use((err, _req, _res, next) => {
                 err.status = 400
             }
 
+            if (error.message === 'null is not a valid date') {
+                error.message = 'Please provide a valid date';
+                err.message = 'Bad Request';
+                err.status = 400
+            }
+
             //route validation errors
             if (error.message.includes('is not a valid integer')) {
                 error.message = 'Please provide a valid integer';
