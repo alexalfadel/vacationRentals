19637ada3f12f68063ac38a32017e1617371c246
Alex Alfadel <alexalfadel@gmail.com>
2023-10-17 15:20:17 -0700

    demo user added

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 // import { Redirect } from "react-router-dom";
@@ -10,7 +10,13 @@ function LoginFormModal() {
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [ disabled, setDisabled ] = useState(true)
   const { closeModal } = useModal();
+
+  useEffect(() => {
+    if (credential.length >= 4 && password.length >= 6) setDisabled(false)
+    else setDisabled(true)
+  }, [credential, password])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,6 +30,31 @@ function LoginFormModal() {
         }
       });
   };
+
+  const demoUser = async () => {
+    // let loggedInUser;
+    const loggedInUser = await dispatch(sessionActions.login({ 
+      credential: 'demo',
+      password: 'password'
+    })).catch( async (res) => {
+          const data = await res.json()
+          console.log('user does not exist')
+        return false;
+        })
+     
+
+    if (!loggedInUser) {
+      await dispatch(sessionActions.signup({
+        email: 'demo@demo.com',
+        username: 'demo',
+        firstName: 'Demo',
+        lastName: 'Account',
+        password: 'password'
+      }))
+      
+    }
+    closeModal();
+  }
 
   return (
     <>
@@ -50,8 +81,9 @@ function LoginFormModal() {
         {errors.credential && (
           <p>{errors.credential}</p>
         )}
-        <button type="submit">Log In</button>
+        <button disabled={disabled} type="submit">Log In</button>
       </form>
+      <p onClick={demoUser}>Demo User</p>
     </>
   );
 }
