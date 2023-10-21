0891c345dfdccba53a517fb815ae970c3e756470
Alex Alfadel <alexalfadel@gmail.com>
2023-10-21 14:41:07 -0700

    manage spots page styled

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -6,6 +6,15 @@ import { restoreUser } from '../../store/session';
 import { Link } from 'react-router-dom'
 import OpenModalButton from '../OpenModalButton';
 import DeleteSpotModal from '../DeleteSpotModal';
+import './ManageSpots.css'
+
+const formatPrice = (price) => {
+    console.log(typeof price)
+    let priceString = price.toString();
+    console.log(priceString)
+    if (!priceString.includes('.')) priceString += '.00'
+    return priceString
+}
 
 function ManageSpots () {
     const history = useHistory();
@@ -41,19 +50,19 @@ function ManageSpots () {
         return (
             // <Link id={`spot${id}`} to={`/spots/${id}`}>
                 <div className='spots-square' id={id}>
-                    <Link id={`spot${id}`} to={`/spots/${id}`}>
-                    <img src={previewImage} alt={description}></img>
-                    <div>
+                    <Link className='spot-tile-link' id={`spot${id}`} to={`/spots/${id}`}>
+                    <img className='spot-image'src={previewImage} alt={description}></img>
+                    <div className='spot-location-reviews'>
                         <p>{city},{state}</p>
                         <p><span><i className="fa-solid fa-star"></i></span>{avgRating}</p>
                     </div>
-                    <p>${price} night</p>
+                    <p className='spot-price'><span className='price'>${formatPrice(price)}</span> night</p>
                     </Link>
                     <div>
-                        <button onClick={() => {
+                        <button className='manage-spots-update' onClick={() => {
                             history.push(`/spots/${id}/edit`)
                         }}>Update</button>
-                        <OpenModalButton buttonText='Delete' modalComponent={<DeleteSpotModal spotId={id} />} />
+                        <button className='manage-spots-update'><OpenModalButton textColor='white' buttonText='Delete' modalComponent={<DeleteSpotModal spotId={id} />} /></button>
 
                     </div>
                 </div>
@@ -65,16 +74,16 @@ function ManageSpots () {
     
 
     return (
-        <>
-            <div>
+        <div className='manage-spots-container'>
+            <div className='manage-spots-header'>
                 <h2>Manage Spots</h2>
                 <button onClick={newSpotClick}>Create a New Spot</button>
             </div>
-            <div>
+            <div className='manage-spot-tile-box'>
                 {loadEachSpotTile}
             </div>
             
-        </>
+        </div>
     )
 }
 
