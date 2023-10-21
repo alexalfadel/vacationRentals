742099eb5375793fe008cef87201ff34d147934e
Alex Alfadel <alexalfadel@gmail.com>
2023-10-21 13:44:12 -0700

    delete review modal styled

--- frontend/src/components/DeleteReviewModal/index.js
+++ frontend/src/components/DeleteReviewModal/index.js
@@ -1,6 +1,7 @@
 import { useModal } from "../../context/Modal";
 import { useDispatch } from 'react-redux'
 import { deleteReviewThunk } from "../../store/spots";
+import './DeleteReviewModal.css'
 
 
 function DeleteReviewModal ({ review }) {
@@ -10,18 +11,18 @@ function DeleteReviewModal ({ review }) {
 
 
     return (
-        <>
+        <div className='delete-review-modal'>
             <h2>Confirm Delete</h2>
             <p>Are you sure you want to delete this review?</p>
-            <div>
-                <button onClick={async () => {
+            <div className='delete-review-buttons-box'>
+                <button id='delete-review' onClick={async () => {
                     console.log('delete button clicked')
                     await dispatch(deleteReviewThunk({id, spotId}))
                     closeModal();
                 }}>Yes (Delete Review)</button>
-                <button onClick={closeModal}>No (Keep Review)</button>
+                <button id='keep-review' onClick={closeModal}>No (Keep Review)</button>
             </div>
-        </>
+        </div>
     )
 }
 
