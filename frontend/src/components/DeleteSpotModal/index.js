d56b74ad0d0e2b6eeab11151232e0c2586cf6386
Alex Alfadel <alexalfadel@gmail.com>
2023-10-21 14:48:55 -0700

    delete spot modal styled

--- frontend/src/components/DeleteSpotModal/index.js
+++ frontend/src/components/DeleteSpotModal/index.js
@@ -1,6 +1,7 @@
 import { useModal } from "../../context/Modal";
 import { deleteSpotThunk, getAllSpotsCurrentUser } from "../../store/spots";
 import { useDispatch } from 'react-redux'
+import './DeleteSpotModal.css'
 
 
 function DeleteSpotModal ({ spotId }) {
@@ -9,18 +10,18 @@ function DeleteSpotModal ({ spotId }) {
 
 
     return (
-        <>
+        <div className='delete-spot-modal'>
             <h2>Confirm Delete</h2>
             <p>Are you sure you want to remove this spot from the listings?</p>
-            <div>
-                <button onClick={async () => {
+            <div className='delete-spot-buttons-box'>
+                <button id='delete-spot' onClick={async () => {
                      await dispatch(deleteSpotThunk(spotId));
                      dispatch(getAllSpotsCurrentUser())
                      closeModal();
                 }}>Yes (Delete Spot)</button>
-                <button onClick={closeModal}>No (Keep Spot)</button>
+                <button id='keep-spot' onClick={closeModal}>No (Keep Spot)</button>
             </div>
-        </>
+        </div>
     )
 }
 
